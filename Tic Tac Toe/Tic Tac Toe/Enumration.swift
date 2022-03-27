3cbdc02bb495525ac6bc8374f1a06a64b57a50ae
Purvesh <purvesh.d@simformsolutions.com>
2022-03-27 21:22:20 +0530

    feature/game_logical_part

---
+++ Tic Tac Toe/Tic Tac Toe/Enumration.swift
@@ -0,0 +1,15 @@
+//
+//  Enumration.swift
+//  Tic Tac Toe
+//
+//  Created by Purvesh Dodiya on 27/03/22.
+//
+
+import Foundation
+
+//Enum
+enum TypeOfTurn: String {
+    case x
+    case o
+    case `default`
+}
